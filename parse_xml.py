f3448d021eb0e2039d673b2e4fa26991d1e7a5b7
Emily Sheng <ewsheng@isi.edu>
2016-07-23 18:43:13 -0700

    fixed template code to run parse_xml.py

--- parse_xml.py
+++ parse_xml.py
@@ -15,9 +15,21 @@ folder_with_xml_files = sys.argv[1]
 # This variable stores the name of the file we will write our collected sentences to.
 output_file_name = sys.argv[2]
 
+# Put all sentences in list_of_sentences
+list_of_sentences = []
+
+# Open the output file.
+output_file = open(output_file_name, 'w')
+
 # Iterate over each file in the folder.
 for file_name in os.listdir(folder_with_xml_files):
+
+	# Ignore files that end with '.sci.xml'
 	if not file_name.endswith('.sci.xml'):
+
+		print '========================='
+		print 'File:', file_name
+
 		# Open the file and get the root of the file.
 		element_tree = ElementTree.parse(folder_with_xml_files+'/'+file_name)
 
@@ -28,38 +40,43 @@ for file_name in os.listdir(folder_with_xml_files):
 		### TODO ###
 
 		# Step 1. What are the names and paths of the XML tags we are interested in? [Done]
-		# root -> title
-		# root -> content -> sections -> section -> title
-		# root -> content -> sections -> section -> paragraphs -> paragraph -> sentence -> text
+		# (A) root -> title
+		# (B) root -> content -> sections -> section -> title
+		# (C) root -> content -> sections -> section -> paragraphs -> paragraph -> sentence -> text
 
 		# Step 2. How can we use the "find" and "findall" functions to find the content in the tags we are interested in? [Fill in code]
-		# Put all sentences in list_of_sentences
-		list_of_sentences = []
-		print file_name
-		print root
-		print root.find["title"]
+		# (A)
 		title = root.find("title").text
-		list_of_sentences.append(title)
+		# Only add title to list_of_sentences if title exists
+		if title:
+			list_of_sentences.append(title)
+
 		section_list = root.find('content').find('sections').findall('section')
 		for S in section_list: 
-			section_title = S.find('title').text	
-			list_of_sentences.append(section_title)
+			# (B)
+			section_title = S.find('title').text
+			# Only add section_title to list_of_sentences if section_title exists
+			if section_title:
+				list_of_sentences.append(section_title)
 			paragraphs = S.find('paragraphs')
 			paragraph_list = paragraphs.findall('paragraph')
 			for P in paragraph_list:
 				for x in P:
+					# (C)
 					text = x.find('text').text
-					list_of_sentences.append(text)
+					# Only add text to list_of_sentences if text exists
+					if text:
+						list_of_sentences.append(text)
 
 	  
-		# Step 3. Write all sentences out to a new file. [Done]
-		# Open the output file.
-		output_file = open(output_file_name, 'w')
+# Step 3. Write all sentences out to a new file. [Done]
+# for each sentence in list_of_sentences
+for sentence in list_of_sentences:
 
-		# for each sentence in list_of_sentences
-		for sentence in list_of_sentences:
+	# Print the sentence
+	print 'sentence:', sentence
 
-			# Write a sentence to the file.
-			output_file.write(sentence + '\n')
+	# Write a sentence to the file.
+	output_file.write(sentence.encode('utf-8') + '\n')
 
 
